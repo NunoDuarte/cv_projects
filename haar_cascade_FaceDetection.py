828a91652d419a12335d7730512a023e3c486590
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2016-12-26 01:53:15 +0000

    Created my own haar cascade to detect my ipad; Doesnt work that well; The main goal was to learn how to use cascade; May look into why it is not working

--- haar_cascade_FaceDetection.py
+++ haar_cascade_FaceDetection.py
@@ -1,9 +1,12 @@
 import numpy as np
 import cv2
 
+# This is the code for existing cascade already available online
+# now we are going to do it for our created haar cascade
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
+watch_cascade =  cv2.CascadeClassifier('ipad-cascade-10stages.xml')
 cap = cv2.VideoCapture(0)
 
 while 1:
@@ -11,14 +14,21 @@ while 1:
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-    for (x,y,w,h) in faces:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = img[y:y+h, x:x+w]
+    watches = watch_cascade.detectMultiScale(gray, 20, 20)
 
-        eyes = eye_cascade.detectMultiScale(roi_gray)
-        for (ex, ey, ew, eh) in eyes:
-            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+    for (x,y,w,h) in watches:
+        #cv2.rectangle(img, (x,y), (x+w, y+h), (255,255,0), 2)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(img, 'Ipad', (x-w, y-h), font, 0.5, (0,255,255), 2, cv2.CV_AA)
+
+    # for (x,y,w,h) in faces:
+    #     cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    #     roi_gray = gray[y:y+h, x:x+w]
+    #     roi_color = img[y:y+h, x:x+w]
+    #
+    #     eyes = eye_cascade.detectMultiScale(roi_gray)
+    #     for (ex, ey, ew, eh) in eyes:
+    #         cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
 
     cv2.imshow('img', img)
     k = cv2.waitKey(30) & 0xff
