75f3e28435ceeec8437787dd9c67aaf46419be8d
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2017-12-04 16:49:19 +0000

    begin working on detecting the markers on the table

---
+++ shapeDetector/center_of_shape.py
@@ -0,0 +1,27 @@
+# import the necessary packages
+import argparse
+import imutils
+import cv2
+
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+        help="path to the input image")
+args = vars(ap.parse_args())
+print(args["image"])
+
+# load the image, convert it to grayscale, blur it slightly,
+# and threshold it
+image = cv2.imread(args["image"])
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+thresh =cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
+
+cv2.imshow('image1',image)
+cv2.imshow('image2',gray)
+cv2.imshow('image3',blurred)
+cv2.imshow('image4',thresh)
+cv2.waitKey(0)
+#cv2.destroyAllWindows()
+
