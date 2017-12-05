e3435e4fbafcfbc346dd8875fa3d506550ae6ccd
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2017-12-05 15:35:19 +0000

    update all files to work with standard image 'shapes_and_colors.png'; add circle_shape.py

--- shapeDetector/main.py
+++ shapeDetector/main.py
@@ -1,5 +1,6 @@
 # import the necessary packages
 from circle_shape import ShapeDetector
+from center_of_shape import CenterShape
 import argparse
 import imutils
 import cv2
@@ -10,29 +11,12 @@ ap.add_argument("-i", "--image", required=True,
                 help="path to the input image")
 args = vars(ap.parse_args())
 
-# load the image and resize it to a smaller factor so that
-# the shapes can be approximated better
 image = cv2.imread(args["image"])
-resized = imutils.resize(image, width=750)
-ratio = image.shape[0] / float(resized.shape[0])
 
-# convert the resized image to grayscale, blur it slightly,
-# and threshold it
-gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
-blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, \
-                                cv2.THRESH_BINARY_INV, 11, 2)
-
-cv2.imshow('image4', thresh)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-# find contours in the thresholded image and initialize the
-# shape detector
-cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-                        cv2.CHAIN_APPROX_SIMPLE)
-cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+cs = CenterShape()
 sd = ShapeDetector()
+
+cnts, ratio = cs.center(image)
 
 # loop over the contours
 a = 1
@@ -43,21 +27,25 @@ for c in cnts:
     # compute the center of the contour, then detect the name of the
     # shape using only the contour
     M = cv2.moments(c)
-    cX = int((M["m10"] / M["m00"]) * ratio)
-    cY = int((M["m01"] / M["m00"]) * ratio)
-    shape = sd.detect(c)
+    if M["m00"] != 0:
+        cX = int((M["m10"] / M["m00"]) * ratio)
+        cY = int((M["m01"] / M["m00"]) * ratio)
+        shape = sd.detect(c)
 
-    # multiply the contour (x, y)-coordinates by the resize ratio,
-    # then draw the contours and the name of the shape on the image
-    c = c.astype("float")
-    c *= ratio
-    c = c.astype("int")
-    cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
-    cv2.putText(image, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
-                0.5, (255, 255, 255), 2)
+        # multiply the contour (x, y)-coordinates by the resize ratio,
+        # then draw the contours and the name of the shape on the image
+        c = c.astype("float")
+        c *= ratio
+        c = c.astype("int")
+        cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+        cv2.putText(image, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5, (255, 255, 255), 2)
 
-    # show the output image
-    cv2.imshow("Image", image)
-    cv2.waitKey(0)
+        # show the output image
+        resized = imutils.resize(image, width=750)
+        cv2.imshow("Image_shapes", resized)
+        cv2.waitKey(0)
+
+    a=a+1
 
 
