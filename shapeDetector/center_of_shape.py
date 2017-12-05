dca4bd059a11c695e7d4331d9a8c3ee6f6fefd0c
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2017-12-05 14:25:39 +0000

    detect the center marker of the white blobs; it is not appropriate to this image because our markers have a black dot inside the white blob; it works nonetheless

--- shapeDetector/center_of_shape.py
+++ shapeDetector/center_of_shape.py
@@ -3,11 +3,10 @@ import argparse
 import imutils
 import cv2
 
-
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
-        help="path to the input image")
+                help="path to the input image")
 args = vars(ap.parse_args())
 print(args["image"])
 
@@ -16,8 +15,8 @@ print(args["image"])
 image = cv2.imread(args["image"])
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-thresh = cv2.adaptiveThreshold(blurred,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
-            cv2.THRESH_BINARY,11,2)
+thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, \
+                                cv2.THRESH_BINARY, 11, 2)
 
 # cv2.imshow('image1', image)
 # cv2.imshow('image2', gray)
@@ -26,3 +25,30 @@ cv2.imshow('image4', thresh)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+# find contours in the thresholded image
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+                        cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+
+# loop over the contours
+a = 1
+print(len(cnts))
+for c in cnts:
+    if a !=1:
+
+        print(a)
+        # compute the center of the contour
+        M = cv2.moments(c)
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+
+        # draw the contour and center of the shape on the image
+        cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+        cv2.circle(image, (cX, cY), 7, (255, 255, 255), -1)
+        cv2.putText(image, "center", (cX - 20, cY - 20),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
+        # show the image
+        cv2.imshow("Image", image)
+        cv2.waitKey(0)
+    a=a+1
