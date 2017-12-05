4c32373b00bbb20af5e2d9b4cb764db8d4ea15f4
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2017-12-05 13:55:43 +0000

    correct error that made segmentation fault

--- shapeDetector/center_of_shape.py
+++ shapeDetector/center_of_shape.py
@@ -18,10 +18,10 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 thresh =cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
 
-cv2.imshow('image1',image)
-cv2.imshow('image2',gray)
-cv2.imshow('image3',blurred)
-cv2.imshow('image4',thresh)
+cv2.imshow('image1', image)
+cv2.imshow('image2', gray)
+cv2.imshow('image3', blurred)
+cv2.imshow('image4', thresh)
 cv2.waitKey(0)
-#cv2.destroyAllWindows()
+cv2.destroyAllWindows()
 
