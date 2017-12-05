ef8b94824107ca57c70cf69ac96646a9ab219d11
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2017-12-05 14:08:37 +0000

    add adaptive Guassian thresholding

--- shapeDetector/center_of_shape.py
+++ shapeDetector/center_of_shape.py
@@ -16,11 +16,12 @@ print(args["image"])
 image = cv2.imread(args["image"])
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-thresh =cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
+thresh = cv2.adaptiveThreshold(blurred,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+            cv2.THRESH_BINARY,11,2)
 
-cv2.imshow('image1', image)
-cv2.imshow('image2', gray)
-cv2.imshow('image3', blurred)
+# cv2.imshow('image1', image)
+# cv2.imshow('image2', gray)
+# cv2.imshow('image3', blurred)
 cv2.imshow('image4', thresh)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
