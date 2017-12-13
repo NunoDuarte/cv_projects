6ef66fc76933d15e82208c8cbff914046e05580e
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2017-12-13 17:38:41 +0000

    update clas of hough_circle.py

--- shapeDetector/hough_circle.py
+++ shapeDetector/hough_circle.py
@@ -3,38 +3,44 @@ import argparse
 import cv2
 import imutils
 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True, help="Path to the image")
-args = vars(ap.parse_args())
+class houghCircle:
+    def __init__(self):
+        pass
 
-# load the image, clone it for output, and then convert it to grayscale
-image = cv2.imread(args["image"])
-image = imutils.resize(image, width=750)
-# cv2.imshow("image", image)
-# cv2.waitKey(0)
-output = image.copy()
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    def construct(self, ):
+        # construct the argument parser and parse the arguments
+        ap = argparse.ArgumentParser()
+        ap.add_argument("-i", "--image", required=True, help="Path to the image")
+        args = vars(ap.parse_args())
 
-# detect circles in the image
-# circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.2, 20)
-circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 100,
-                           param1=100, param2=20, minRadius=0, maxRadius=30)
+        # load the image, clone it for output, and then convert it to grayscale
+        image = cv2.imread(args["image"])
+        image = imutils.resize(image, width=750)
+        # cv2.imshow("image", image)
+        # cv2.waitKey(0)
+        output = image.copy()
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# ensure at least some circles were found
-if circles is not None:
-    # convert the (x, y) coordinates and radius of the circles to integers
-    circles = np.round(circles[0, :]).astype("int")
+        # detect circles in the image
+        # circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.2, 20)
+        circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 100,
+                                   param1=100, param2=20, minRadius=0, maxRadius=30)
 
-    # loop over the (x, y) coordinates and radius of the circles
-    for (x, y, r) in circles:
-        # draw the circle in the output image, then draw a rectangle
-        # corresponding to the center of the circle
-        cv2.circle(output, (x, y), r, (0, 255, 0), 4)
-        cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+        # ensure at least some circles were found
+        if circles is not None:
+            # convert the (x, y) coordinates and radius of the circles to integers
+            circles = np.round(circles[0, :]).astype("int")
 
-    # show the output image
-    cv2.imshow("output", np.hstack([image, output]))
-    cv2.waitKey(0)
+            # loop over the (x, y) coordinates and radius of the circles
+            for (x, y, r) in circles:
+                # draw the circle in the output image, then draw a rectangle
+                # corresponding to the center of the circle
+                cv2.circle(output, (x, y), r, (0, 255, 0), 4)
+                cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
 
+            # show the output image
+            # cv2.imshow("output", np.hstack([image, output]))
+            # cv2.waitKey(0)
+
+        return circles
 
