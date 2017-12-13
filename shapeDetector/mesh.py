3969967613a8a35ee79a063e4815d29e050f5994
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2017-12-13 12:16:41 +0000

    change hough_circle code to a class; add the circles discovered in hough to mesh

--- shapeDetector/mesh.py
+++ shapeDetector/mesh.py
@@ -1,9 +1,16 @@
 from shapeDetector import shapeDetector
+from hough_circle import houghCircle
 
 
 # initialize Algorithm to detect Shapes
 main = shapeDetector()
 contours, shapes, ratio = main.construct()
 
+# initialize Algorithm to detect Circles
+hough = houghCircle()
+circles = hough.construct()
+
+
 print(shapes)
 print(len(contours))
+print(circles)
