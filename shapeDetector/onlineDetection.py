23e5e3b5baa96e509ac9e3f96f3cfd46bc224156
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2017-12-28 12:37:08 +0000

    I found the problem; wrong video file; Now it follows the green marker correctly

--- shapeDetector/onlineDetection.py
+++ shapeDetector/onlineDetection.py
@@ -35,29 +35,27 @@ while (True):
     ret, frame = cap.read()
     # print(frame)
     # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    mesh.mesh(frame)
+    if frame is not None:
+        mesh.mesh(frame)
 
-    # calculate the nearest timestamp for the current frame
-    time = timestamps[i]
-    time_close, ind = find_nearest(timestamps_gaze, float(time))
-    print('time ', time)
-    print('ind', ind)
-    print('time_close', time_close)
+        # calculate the nearest timestamp for the current frame
+        time = timestamps[i]
+        time_close, ind = find_nearest(timestamps_gaze, float(time))
 
-    # use the x, y position of the closest timestamp norm_pos_*
-    pos_x = norm_pos_x[ind+20]
-    pos_y = norm_pos_y[ind+20]
+        # use the x, y position of the closest timestamp norm_pos_*
+        pos_x = norm_pos_x[ind]
+        pos_y = norm_pos_y[ind]
 
-    print(int(float(pos_x)*width))
-    print(int(height - int(float(pos_y)*height)))
-    cv2.circle(frame, (int(float(pos_x)*width), int(height - int(float(pos_y)*height))), 10, (0, 255, 1), thickness=1, lineType=8, shift=0)  # draw circle
+        print(int(float(pos_x)*width))
+        print(int(height - int(float(pos_y)*height)))
+        cv2.circle(frame, (int(float(pos_x)*width), int(height - int(float(pos_y)*height))), 10, (0, 255, 1), thickness=5, lineType=8, shift=0)  # draw circle
 
-    cv2.imshow('frame', frame)
+        cv2.imshow('frame', frame)
     if cv2.waitKey(25) & 0xFF == ord('q'):
         break
 
     i = i + 1
-    cv2.waitKey(0)
+    #cv2.waitKey(0)
 
 cap.release()
 cv2.destroyAllWindows()
