123646b025630a824e28f4f6fc4207711e900cfa
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2018-01-03 18:39:39 +0000

    clean code; remove comments

--- shapeDetector/faceDetector.py
+++ shapeDetector/faceDetector.py
@@ -12,9 +12,6 @@ class faceDetector:
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         blurred = cv2.GaussianBlur(gray, (31, 31), 0)
         thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
-        # thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, \
-        #                                  cv2.THRESH_BINARY_INV, 11, 2)
-        # ret3, thresh = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 
         faces = faceCascade.detectMultiScale(
             thresh,
