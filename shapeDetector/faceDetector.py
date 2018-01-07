557492637baee3eaa9ea63cbc43409a41a7d2fe2
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2018-01-07 12:50:27 +0000

    create a folder of dataset faces to be trained so we recognize each face

--- shapeDetector/faceDetector.py
+++ shapeDetector/faceDetector.py
@@ -1,6 +1,7 @@
 import cv2
 import logging as log
 import datetime as dt
+import os
 
 class faceDetector:
 
@@ -32,4 +33,45 @@ class faceDetector:
             log.info("faces: "+str(len(facesDetect))+" at "+str(dt.datetime.now()))
 
         return anterior, faces
+
+    def prepare_training_data(self, data_folder_path):
+
+        dirs = os.listdir(data_folder_path)
+
+        faces = []
+        labels = []
+
+        for dir_name in dirs:
+            if not dir_name.startswith("s"):
+                continue
+
+            label = int(dir_name.replace("s", ""))
+
+            subject_dir_path = data_folder_path + "/" + dir_name
+
+            subject_images_names = os.listdir(subject_dir_path)
+
+            for image_name in subject_images_names:
+                if image_name.startswith("."):
+                    continue
+
+                image_path = subject_dir_path + "/" + image_name
+
+                image = cv2.imread(image_path)
+
+                cv2.imshow("Training on image...", image)
+                cv2.waitKey(100)
+
+                face, rect = detect_face(image)
+
+                if face is not None:
+                    faces.append(face)
+
+                    labels.append(label)
+
+                    cv2.destroyAllWindows()
+                    cv2.waitKey(1)
+                    cv2.destroyAllWindows()
+
+                return faces, labels
 
