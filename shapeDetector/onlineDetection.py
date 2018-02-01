2e431026ffe89f3cd9ffbf75fb35794ad6ee45b8
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2018-02-01 12:47:24 +0000

    adding frame publisher to our onlineDetection system; it is too slow when adding everything

--- shapeDetector/onlineDetection.py
+++ shapeDetector/onlineDetection.py
@@ -13,7 +13,63 @@ import argparse
 import imutils
 import logging as log
 
-cap = cv2.VideoCapture('world_viz.mp4')
+"""
+Receive world camera data from Pupil using ZMQ.
+Make sure the frame publisher plugin is loaded and confugured to gray or rgb
+"""
+import zmq
+from msgpack import unpackb, packb
+import numpy as np
+import cv2
+
+context = zmq.Context()
+# open a req port to talk to pupil
+addr = '127.0.0.1'  # remote ip or localhost
+req_port = "50020"  # same as in the pupil remote gui
+req = context.socket(zmq.REQ)
+req.connect("tcp://{}:{}".format(addr, req_port))
+# ask for the sub port
+req.send_string('SUB_PORT')
+sub_port = req.recv_string()
+
+# send notification:
+def notify(notification):
+    """Sends ``notification`` to Pupil Remote"""
+    topic = 'notify.' + notification['subject']
+    payload = packb(notification, use_bin_type=True)
+    req.send_string(topic, flags=zmq.SNDMORE)
+    req.send(payload)
+    return req.recv_string()
+
+
+# Start frame publisher with format BGR
+notify({'subject': 'start_plugin', 'name': 'Frame_Publisher', 'args': {'format': 'bgr'}})
+
+# open a sub port to listen to pupil
+sub = context.socket(zmq.SUB)
+sub.connect("tcp://{}:{}".format(addr, sub_port))
+
+# set subscriptions to topics
+# recv just pupil/gaze/notifications
+sub.setsockopt_string(zmq.SUBSCRIBE, 'frame.')
+
+def recv_from_sub():
+    '''Recv a message with topic, payload.
+
+    Topic is a utf-8 encoded string. Returned as unicode object.
+    Payload is a msgpack serialized dict. Returned as a python dict.
+
+    Any addional message frames will be added as a list
+    in the payload dict with key: '__raw_data__' .
+    '''
+    topic = sub.recv_string()
+    payload = unpackb(sub.recv(), encoding='utf-8')
+    extra_frames = []
+    while sub.get(zmq.RCVMORE):
+        extra_frames.append(sub.recv())
+    if extra_frames:
+        payload['__raw_data__'] = extra_frames
+    return topic, payload
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
@@ -62,45 +118,51 @@ with open('gaze_positions_18-12-2017.csv', newline='') as csvfile:
 timestamps = np.load('world_viz_timestamps.npy')
 
 i = 0
-while (True):
-    ret, frame = cap.read()
+while True:
+    topic, msg = recv_from_sub()
+    # file = open('Failed.py', 'w')
 
-    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    if frame is not None:
-        frame = imutils.resize(frame, width=750)
-        height, width, channels = frame.shape
+    if topic == 'frame.world':
+        # file.write(str(msg))
+        frame = np.frombuffer(msg['__raw_data__'][0], dtype=np.uint8).reshape(msg['height'], msg['width'], 3)
+        # cv2.imshow('test', frame)
+        cv2.waitKey(1)
 
-        frame, markers = mesh.mesh(frame)
+        # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        if frame is not None:
+            frame = imutils.resize(frame, width=750)
+            height, width, channels = frame.shape
 
-        frame, pts, ball = ballTracking.tracking(frame, pts, args)
-
-        anterior, faces, facesTrained = face.detecting(frame, anterior, faceCascade)
-        labels = face.predict(frame, face_recognizer, faces, facesTrained)
-
-        # calculate the nearest timestamp for the current frame
-        time = timestamps[i]
-        time_close, ind = find_nearest(timestamps_gaze, float(time))
-
-        # use the x, y position of the closest timestamp norm_pos_*
-        pos_x = norm_pos_x[ind]
-        pos_y = norm_pos_y[ind]
-
-        #print(int(float(pos_x)*width))
-        #print(int(height - int(float(pos_y)*height)))
-        cv2.circle(frame, (int(float(pos_x)*width), int(height - int(float(pos_y)*height))), 10, (0, 255, 1), thickness=5, lineType=8, shift=0)  # draw circle
-        fixation = [(int(float(pos_x)*width)), int(height - int(float(pos_y)*height))]
-
-        # check the gaze behaviour
-        if len(ball) is not 0:
-            gaze.record(time_close, markers, ball, faces, fixation, labels, f)
+            # frame, markers = mesh.mesh(frame)
+            #
+            frame, pts, ball = ballTracking.tracking(frame, pts, args)
+            #
+            # anterior, faces, facesTrained = face.detecting(frame, anterior, faceCascade)
+            # labels = face.predict(frame, face_recognizer, faces, facesTrained)
+            #
+            # # calculate the nearest timestamp for the current frame
+            # time = timestamps[i]
+            # time_close, ind = find_nearest(timestamps_gaze, float(time))
+            # #
+            # # use the x, y position of the closest timestamp norm_pos_*
+            # pos_x = norm_pos_x[ind]
+            # pos_y = norm_pos_y[ind]
+            #
+            # # print(int(float(pos_x)*width))
+            # # print(int(height - int(float(pos_y)*height)))
+            # cv2.circle(frame, (int(float(pos_x) * width), int(height - int(float(pos_y) * height))), 10, (0, 255, 1),
+            #            thickness=5, lineType=8, shift=0)  # draw circle
+            # fixation = [(int(float(pos_x) * width)), int(height - int(float(pos_y) * height))]
+            #
+            # # check the gaze behaviour
+            # if len(ball) is not 0:
+            #     gaze.record(time_close, markers, ball, faces, fixation, labels, f)
 
         cv2.imshow('frame', frame)
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        break
 
     i = i + 1
-    #cv2.waitKey(0)
+    # cv2.waitKey(0)
 
 gaze.close(f)
-cap.release()
+# cap.release()
 cv2.destroyAllWindows()
