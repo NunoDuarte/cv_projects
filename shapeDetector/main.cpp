406807bcaa870fde2de9b8ead3bed8b84a271ee4
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2018-02-05 16:20:26 +0000

    clean code; add thread to send my data to yarp

--- shapeDetector/main.cpp
+++ shapeDetector/main.cpp
@@ -1,37 +1,26 @@
-//#include "opencv2/opencv.hpp"
-//#include <opencv2/core/core.hpp>
-//#include <opencv2/imgproc/imgproc.hpp>
-//#include <opencv2/highgui/highgui.hpp>
 #include <iostream>
 #include <sstream>
-#include <lsl_cpp.h>
-#include <iostream>
-#include <fstream>
-#include "json.hpp"
-
-#include "yarp/os/all.h"
-
-using namespace std;
-using namespace yarp::os;
-//using namespace cv;
-
-//#define LSL_OT
-#define LSL_PL1
-//#define LSL_PL2
-
-
-#include <iostream>
 #include <fstream>
 #include <thread>         // std::thread
 #include <vector>
+#include "lsl_cpp.h"
+#include "json.hpp"
+#include "yarp/os/all.h"
+
+//#define LSL_OT
+#define LSL_PL1
+#define LSL_PL1YARP
+//#define LSL_PL2
 
 using namespace std;
+using namespace yarp::os;
 // for convenience
 using json = nlohmann::json;
 time_t rawtime;
 
 double sample_ot[15];
 string sample_pl1;
+string sample_pl1yarp;
 string sample_pl2;
 std::string strtse01 = "-1";
 std::string strtse11 = "-1";
@@ -41,9 +30,7 @@ std::string strtse12 = "-1";
 char ot_new_data_ready=0;
 char pl1_new_data_ready=0;
 char pl2_new_data_ready=0;
-
-
-
+char pl1yarp_new_data_ready=0;
 
 void optitrack_capture()
 {
@@ -104,21 +91,14 @@ void pl1_capture()
 {
   #ifdef LSL_PL1
       double ts_pl1;
-      ofstream sample_pl1_file;
-      string file_name_pl1;
-      double json_ts;
-      string json_np;
-      char input_str[10000];
-      double ts_e0,ts_e1;
       float sample[3];
+      ofstream sample_pl1_file;
       const char *channels[] = {"TS","Nx","Ny"};
 
       std::size_t found, foundtse0, foundtse1, foundtse2, foundcfe0, foundcfe1, foundcfe2;
 
-
       string name = "NormPose2IP";
       string type = "Gaze Norm pose";
-
 
       lsl::stream_info info(name,type,3,100,lsl::cf_float32,string(name)+=type);
       info.desc().append_child_value("manufacturer", "Vislab");
@@ -130,108 +110,69 @@ void pl1_capture()
 
       lsl::stream_outlet outlet(info);
 
-
-      Network yarp;
-      BufferedPort<Bottle> port;
-      Bottle& output = port.prepare();
-      port.open("/pupil_gaze_tracker");
-
       json j_obj;
-
-      sample_pl1_file.precision(17);
 
       vector<lsl::stream_info> results = lsl::resolve_stream("name","Gaze Python Representation");
       lsl::stream_inlet inlet_pl1(results[0]);
-
-      file_name_pl1 = "samples_pl1";
-      file_name_pl1.append(ctime (&rawtime));
-      file_name_pl1.append(".csv");
-      sample_pl1_file.open (file_name_pl1.c_str());
-
-
-    /*while (true) {
-        cout << "waiting for input" << endl;
-        Bottle *input = port.read();
-        if (input!=NULL) {
-            cout << "got " << input->toString().c_str() << endl;
-            double total = 0;
-            for (int i=0; i<input->size(); i++) {
-                total += input->get(i).asDouble();
-            }
-            Bottle& output = port.prepare();
-            output.clear();
-            output.addString("total");
-            output.addDouble(total);
-            cout << "writing " << output.toString().c_str() << endl;
-            port.write();
-        }
-    }
-    return 0;*/
 
       while(1)
       {
           ts_pl1 = inlet_pl1.pull_sample(&sample_pl1,1,0.009);//wait up to 9ms
 
-          //cout << "got: " << sample_pl1 << endl;
-
-
           ReplaceStringInPlace(sample_pl1, "0:", "'0':");
           ReplaceStringInPlace(sample_pl1, "1:", "'1':");
           std::replace( sample_pl1.begin(), sample_pl1.end(), '\'', '"'); // replace all 'x' to 'y'
 
-          sample_pl1_file << sample_pl1 << endl;
-            //cout << "sample:" << sample_pl1 << ":end";
           if(sample_pl1.length()>1)//if within 9ms data from glasses arrived
           {
-
-              //json j = "{ \"happy\": true, \"pi\": 3.141 }"_json;
               j_obj = json::parse(sample_pl1);
 
-              /*if(sample_pl1.length()>1)
-              {
-                  found = sample_pl1.find("'timestamp': ");
-                  foundtse0 = sample_pl1.find("'timestamp': ",found+1)+12;
-                  foundcfe0 = sample_pl1.find(",",foundtse0+1)-1;
-
-                  foundtse1 = sample_pl1.find("'timestamp': ",foundcfe0+1)+12;
-                  foundcfe1 = sample_pl1.find(", 'projected_sphere':",foundtse1+1)-1;
-
-                  foundtse2 = sample_pl1.find("'timestamp': ",foundcfe1+1)+12;
-                  foundcfe2 = sample_pl1.find(", 'projected_sphere':",foundtse2+1)-1;
-
-
-                  if(foundtse0<foundcfe0 && foundtse1<foundcfe1)
-                  {
-                    strtse01 = sample_pl1.substr(foundtse0+1,13);
-                    strtse11 = sample_pl1.substr(foundtse1+1,13);
-                  }
-              }*/
-              //json_np = j_obj["timestamp"];//[0]["norm_pos"];
-              //json_ts = j_obj;//[0]["timestamp"];
-
-              //cout << "json " << j_obj["timestamp"]  << " " << j_obj["norm_pos"][0] << " " << j_obj["norm_pos"][1] << "endjson";
-
               pl1_new_data_ready = 1;
-              output = port.prepare();
-              output.clear();
-              output.addDouble(j_obj["timestamp"].get<double>());
-              output.addDouble(j_obj["norm_pos"][0].get<double>());
-              output.addDouble(j_obj["norm_pos"][1].get<double>());
-
               sample[0] = (float)(j_obj["timestamp"].get<double>());
               sample[1] = (float)(j_obj["norm_pos"][0].get<double>());
               sample[2] = (float)(j_obj["norm_pos"][1].get<double>());
               outlet.push_sample(sample);
-//              output.addString(j_obj);
-//              cout << "writing " << output.toString().c_str() << endl;
-              port.write();
           }
       }
-      sample_pl1_file.close();
-
   #else
     while(1)
         pl1_new_data_ready = 1;
+  #endif
+}
+
+void pl1_yarp()
+{
+  #ifdef LSL_PL1YARP
+      double ts_pl1;
+      Network yarp;
+      BufferedPort<Bottle> port;
+
+      Bottle& output = port.prepare();
+      port.open("/pupil_gaze_tracker");
+
+      cout << "1111111111111111..." << endl;
+
+      vector<lsl::stream_info> results = lsl::resolve_stream("name","GazePose");
+      lsl::stream_inlet inlet_pl1(results[0]);
+
+      while(1)
+      {
+          cout << "2222222222222222..." << endl;
+          if (ts_pl1 = inlet_pl1.pull_sample(&sample_pl1yarp,3,0.009))//wait up to 9ms
+          {
+
+              pl1_new_data_ready = 1;
+              output = port.prepare();
+              output.clear();
+              output.addDouble(sample_pl1yarp[0]);
+              output.addDouble(sample_pl1yarp[1]);
+              output.addDouble(sample_pl1yarp[2]);
+              port.write();
+           }
+      }
+  #else
+    while(1)
+        pl1yarp_new_data_ready = 1;
   #endif
 }
 
@@ -328,6 +269,7 @@ int main(int argc, char *argv[])
 
     threads.push_back(std::thread(optitrack_capture));
     threads.push_back(std::thread(pl1_capture));
+    threads.push_back(std::thread(pl1_yarp));
     threads.push_back(std::thread(pl2_capture));
 
     sample_file <<"header";
@@ -341,10 +283,11 @@ int main(int argc, char *argv[])
     while(1)
     {
         time(&rawtime);
-        if(ot_new_data_ready && pl1_new_data_ready && pl2_new_data_ready)
+        if(ot_new_data_ready && pl1_new_data_ready && pl1yarp_new_data_ready && pl2_new_data_ready)
         {
             ot_new_data_ready = 0;
             pl1_new_data_ready = 0;
+            pl1yarp_new_data_ready = 0;
             pl2_new_data_ready = 0;
 
             for (unsigned c=0;c<11;c++)
@@ -371,10 +314,7 @@ int main(int argc, char *argv[])
         thread.join();
     }
 
-
-
     return 0;
-
 }
 
 
