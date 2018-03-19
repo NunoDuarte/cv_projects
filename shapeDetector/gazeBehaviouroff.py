b241a4aa15f99225caebd721ec7c21a95cf20d9c
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2018-03-19 14:51:13 +0000

    increase threshold

--- shapeDetector/gazeBehaviouroff.py
+++ shapeDetector/gazeBehaviouroff.py
@@ -7,17 +7,17 @@ class gazeBehaviour:
         pass
 
     def open(self):
-        f = open('gaze-CENTER-PERSON.txt', 'w')
+        f = open('gaze-CENTER-MARKER.txt', 'w')
         return f
 
     def record(self, timestamp, markers, ball, faces, fixation, labels, file):
-        epsilon = 40  # the threshold in pixels allowed
+        epsilon = 80  # the threshold in pixels allowed
 
         distX = fixation[0] - ball[0][0]
         distY = fixation[1] - ball[0][1]
 
         if math.sqrt(pow(distX, 2) + pow(distY, 2)) < epsilon:
-            file.write('time: '+str(timestamp)+' Fixation intercepts Ball: '+str(ball)+'\n')
+            file.write('time: '+str(timestamp)+' Fixation intercepts Ball: '+str(ball[0])+'\n')
             print("Ball")
 
         for marker in markers:
@@ -41,7 +41,7 @@ class gazeBehaviour:
             cH = face[1] + face[3]
 
             if cX - 30 < fixation[0] < cW + 30 and cY - 30 < fixation[1] < cH + 30:
-                file.write('time: ' + str(timestamp) + ' Fixation intercepts ' + labels[i] + '´s Face: ' + str(face) + '\n')
+                file.write('time: ' + str(timestamp) + ' Fixation intercepts ' + labels[i] + '´s_Face: ' + str(face) + '\n')
                 print("Face")
 
             i = i+1
