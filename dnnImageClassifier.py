b41e16b596d8ebe168b1ddc14c0a7a6be9dd780b
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2018-06-09 02:39:31 +0100

    get most probable object in an image

--- dnnImageClassifier.py
+++ dnnImageClassifier.py
@@ -29,3 +29,33 @@ blob = cv2.dnn.blobFromImage(image, 1, (224, 224), (104, 117, 123))
 # load our serialized model from disk
 print("[INFO] loading model...")
 net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+
+# set the blob as input to the network and perform a forward-pass to
+# obtain our output classification
+net.setInput(blob)
+start = time.time()
+preds = net.forward()
+end = time.time()
+print("[INFO] classification took {:.5} seconds".format(end - start))
+
+# sort the indexes of the probabilities in descending order (higher
+# probabilitiy first) and grab the top-5 predictions
+idxs = np.argsort(preds[0])[::-1][:5]
+
+# loop over the top-5 predictions and display them
+for (i, idx) in enumerate(idxs):
+    # draw the top prediction on the input image
+    if i == 0:
+        text = "Label: {}, {:.2f}%".format(classes[idx],
+                                           preds[0][idx] * 100)
+        cv2.putText(image, text, (5, 25), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.7, (0, 0, 255), 2)
+
+    # display the predicted label + associated probability to the
+    # console
+    print("[INFO] {}. label: {}, probability: {:.5}".format(i + 1,
+                                                            classes[idx], preds[0][idx]))
+
+# display the output image
+cv2.imshow("Image", image)
+cv2.waitKey(0)
