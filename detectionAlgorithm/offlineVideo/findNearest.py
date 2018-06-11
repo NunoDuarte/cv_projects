515bce73cc8867bbe8bbf3be92695859463336d8
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2018-06-11 18:52:38 +0100

    fix PEP8 code rules

--- detectionAlgorithm/offlineVideo/findNearest.py
+++ detectionAlgorithm/offlineVideo/findNearest.py
@@ -2,7 +2,7 @@ import math
 import numpy as np
 
 
-def find_nearest(array, value):
+def findNearest(array, value):
     idx = np.searchsorted(array, value, side="left")
     if idx > 0 and (idx == len(array) or math.fabs(value - array[idx-1]) < math.fabs(value - array[idx])):
         return array[idx-1], idx
