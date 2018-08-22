baff9e5ec762361daf85dc6d8cfb50749d6e0d2d
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2018-08-22 17:55:21 +0100

    make the face detection and eye detection similar to the cpp haar classifier in cpp

--- cpp/detectionAlgorithm/gazeDetection/multithreading.cpp
+++ cpp/detectionAlgorithm/gazeDetection/multithreading.cpp
@@ -108,24 +108,31 @@ void task2(string msg)
 void detectAndDisplay( Mat frame )
 {
 	std::vector<Rect> faces;
-	Mat frame_gray;
+	Mat gray, blurred, thresh;
 
-	cvtColor( frame, frame_gray, CV_BGR2GRAY );
-	equalizeHist( frame_gray, frame_gray );
+	// cv2 - convert RGB to Gray scale
+	cvtColor( frame, gray, COLOR_BGR2GRAY );
+	// cv2 - blur the gray image
+	GaussianBlur( gray, blurred, Size( 31,31), 0);
+	// cv2 - Threshold the blurred image
+	threshold( blurred, thresh, 127, 255, THRESH_TOZERO);
+
+	//cvtColor( frame, frame_gray, CV_BGR2GRAY );
+	//equalizeHist( frame_gray, frame_gray );
 
 	//-- Detect faces
-	face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(30, 30) );
+	face_cascade.detectMultiScale( gray, faces, 1.1, 1, 0|CV_HAAR_SCALE_IMAGE);
 
 	for( size_t i = 0; i < faces.size(); i++ )
 	{
 		Point center( faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5 );
 		ellipse( frame, center, Size( faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
 
-		Mat faceROI = frame_gray( faces[i] );
+		Mat faceROI = gray( faces[i] );
 		std::vector<Rect> eyes;
 
 		//-- In each face, detect eyes
-		eyes_cascade.detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CV_HAAR_SCALE_IMAGE, Size(30, 30) );
+		eyes_cascade.detectMultiScale( faceROI, eyes);
 
 		for( size_t j = 0; j < eyes.size(); j++ )
 		{
