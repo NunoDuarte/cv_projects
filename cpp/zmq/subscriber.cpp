9b7be2cec69ee1f84df8f95ad925314f3260ede3
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2018-12-04 10:11:58 +0000

    remove lines of code commented

--- cpp/zmq/subscriber.cpp
+++ cpp/zmq/subscriber.cpp
@@ -8,7 +8,7 @@ int main()
     zmq::context_t context(1);
     std::cout << 1 << std::endl;
     zmq::socket_t subscriber(context, ZMQ_SUB);
-    subscriber.connect("tcp://127.0.0.1:35607"); //43597
+    subscriber.connect("tcp://127.0.0.1:45141"); //43597
     std::cout << 2 << std::endl;
     subscriber.setsockopt(ZMQ_SUBSCRIBE, "frame.world", 11);
 
@@ -63,51 +63,33 @@ multipart.send(*Sub2DSocket);
 
         zmq::message_t msg;
         subscriber.recv(&msg);
-/*        std::string msg_str = std::string(static_cast<char*>(msg.data()), msg.size());
-        std::cout << "Received '" << msg_str << "'" << std::endl;
-
-        // serializes this object.
-        std::vector<std::string> vec;
-        vec.push_back(msg_str); */
-/*        vec.push_back("MessagePack");
-
-        // serialize it into simple buffer.
-        msgpack::sbuffer sbuf;
-        msgpack::pack(sbuf, vec);;
-*/
-
-/*
-	msgpack::unpacked unpacked_body;
-	msgpack::unpack(&unpacked_body, static_cast<const char*>(env.data()), env.size());
-	unpacked_body.get().convert(&data);
-*/
 
 	std::string msg_str = std::string(static_cast<char*>(msg.data()), msg.size());
-
 	msgpack::object_handle oh = msgpack::unpack(msg_str.data(), msg_str.size());
 	msgpack::object obj = oh.get();
 	std::cout << obj << std::endl;
 
-/*        // deserialize it.
-        msgpack::object_handle oh = msgpack::unpack(sbuf.data(), sbuf.size());
+    }
 
-        // print the deserialized object.
-        msgpack::object obj = oh.get();
-        std::cout << obj << std::endl;  //=> ["Hello", "MessagePack"]
-*/
-}
-    for(int i=0; i<100; i++)
+
+    for(int i=0; i<1; i++)
     {
 
         zmq::message_t msg;
+
         subscriber.recv(&msg);
 	std::string msg_str = std::string(static_cast<char*>(msg.data()), msg.size());
-
 	msgpack::object_handle oh = msgpack::unpack(msg_str.data(), msg_str.size());
 	msgpack::object obj = oh.get();
 	std::cout << obj << std::endl;
 
         subscriber.recv(&msg);
+	msg_str = std::string(static_cast<char*>(msg.data()), msg.size());
+	oh = msgpack::unpack(msg_str.data(), msg_str.size());
+	obj = oh.get();
+	std::cout << obj << std::endl;
+
+        subscriber.recv(&msg,1);
 	msg_str = std::string(static_cast<char*>(msg.data()), msg.size());
 	oh = msgpack::unpack(msg_str.data(), msg_str.size());
 	obj = oh.get();
